27f5090d8057bbbdcf91fc5fb8e9fd3c1bad62bd
Diego Ribeiro <diegoribeiro@MacBook-Pro-de-Diego.local>
2024-05-12 19:59:59 -0300

    create initial tests.

--- ArticlesApp/ArticlesAppTests/ArticlesAppTests.swift
+++ ArticlesApp/ArticlesAppTests/ArticlesAppTests.swift
@@ -8,29 +8,73 @@
 import XCTest
 @testable import ArticlesApp
 
-final class ArticlesAppTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+final class ArticlesPresenterTests: XCTestCase {
+    
+    class MockDataProvider: DataProvider {
+        override func fetchAllArticles() async throws -> ArticlesModel {
+            return ArticlesModel(articles: [])
+        }
+        
+        override func fetchImageData(imageURLString: String) async throws -> Data {
+            return Data()
         }
     }
+    
+    var presenter: ArticlesPresenter!
+    var mockViewDelegate: MockArticlesPresenterViewDelegate!
+    var mockCoordinatorDelegate: MockArticlesPresenterCoordinatorDelegate!
+    
+    override func setUp() {
+        super.setUp()
+        let mockService = MockDataProvider()
+        presenter = ArticlesPresenter(service: mockService)
+        mockViewDelegate = MockArticlesPresenterViewDelegate()
+        mockCoordinatorDelegate = MockArticlesPresenterCoordinatorDelegate()
+        presenter.viewDelegate = mockViewDelegate
+        presenter.coordinatorDelegate = mockCoordinatorDelegate
+    }
+    
+    @MainActor func testFetchArticles() {
+        presenter.fetchArticles()
+        XCTAssertTrue(mockViewDelegate.isLoadingCalled)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 4, execute: {
+            XCTAssertTrue(self.mockViewDelegate.reloadTableViewCalled)
+            XCTAssertTrue(self.mockViewDelegate.isLoadingCalled)
+        })
 
+    }
+    
+    func testNumberOfArticles() {
+        XCTAssertEqual(presenter.numberOfArticles, 0) // Deve ser 0 inicialmente
+        presenter.articlesList = ArticlesModel(articles: [])
+        XCTAssertEqual(presenter.numberOfArticles, 0)
+    }
+    
+}
+
+//MARK: - Mock TO ArticlesPresenterViewDelegate
+class MockArticlesPresenterViewDelegate: ArticlesPresenterViewDelegate {
+    var isLoadingCalled = false
+    var reloadTableViewCalled = false
+    
+    func reloadTableView() {
+        reloadTableViewCalled = true
+    }
+    
+    func loadingView(isLoading: Bool) {
+        isLoadingCalled = true
+    }
+}
+
+//MARK: - Mock TO ArticlesPresenterCoordinatorDelegate
+class MockArticlesPresenterCoordinatorDelegate: ArticlesPresenterCoordinatorDelegate {
+    var goToDetailViewCalled = false
+    var article: Article?
+    var poster: Data?
+    
+    func goToDetailView(article: Article, poster: Data?) {
+        goToDetailViewCalled = true
+        self.article = article
+        self.poster = poster
+    }
 }
